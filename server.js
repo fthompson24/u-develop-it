6b95c8950a6777fa820cb8e23bb792c66a24a717
fthompson24 <fraserjthompson@gmail.com>
2021-03-22 20:19:14 -0500

    created GET, DELETE routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,12 +8,90 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//code that will connect the application to the SQLite database
+// We created a new object, db. This instance was created with the election.db file.
+// The callback function informs us if there's an error in the connection.
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+
+    console.log('Connected to the election database.');
+});
+
+// GET all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err,rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// Get single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+});
+
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+        res.json({
+            message: 'Successfully deleted!',
+            changes: this.changes        
+        });
+    });
+});
+
+// CREATE a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// //ES5 function, not arrow function, to use this
+// db.run(sql, params, function(err, result) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result, this.lastID);
+// });
+
 //Default response for any other request (NOT FOUND) Catch all
 app.use((req, res) => {
     res.status(404).end();
 });
 
-//start express.js server on port 3001. Place at bottom of server.js
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// To ensure that the Express.js server doesn't start before the
+// connection to the database has been established,
+// let's wrap the Express.js server connection located at the
+// bottom of the server.js file in an event handler
+db.on('open', () =>{
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
