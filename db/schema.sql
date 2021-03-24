cf234a7e398b3d2710403f662bebc87488e62fe7
fthompson24 <fraserjthompson@gmail.com>
2021-03-23 20:44:06 -0500

    redo

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,6 @@
---Create candidates table with headers of ID (sql auto sets the ID), first name, last name, and industry connected
 CREATE TABLE candidates (
     id INTEGER PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     industry_connected BOOLEAN NOT NULL
-);
-
---Create parties table with headers of ID, name, and description
-CREATE TABLE parties (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    description TEXT
 );
